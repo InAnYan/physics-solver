78aa12e8f70a182895123d7801a118fc32970db2
ruslan <ruslanpopov1512@gmail.com>
2024-01-28 14:31:57 +0200

    added new formulas

--- physics_solver/math/formulas.py
+++ physics_solver/math/formulas.py
@@ -1,26 +1,61 @@
-from sympy import sin
+from sympy import sin, sqrt, cos
 
 from physics_solver.math.formula import Formula
 from physics_solver.math.variables import *
 
-formulas = [
-    Formula(P, (I ** 2) * R),
-    Formula(v, S / t),
-    Formula(D, 1 / F, context={'converging'}),
-    Formula(v, lam / T),
-    Formula(M, F * d),
-    Formula(ro, m / V),
-    Formula(F, B * I * l * sin(a)),
-    Formula(E, I / (h ** 2)),
-    Formula(T, t / N),
-    Formula(nu, N / t),
-    Formula(T, 1 / nu),
-    Formula(nu, 1 / T),
-    Formula(F, m * g),
-    Formula(P, m * g),
-    Formula(S, S ** 2, context={'square'}),  # A hack.
-    Formula(p, F / S),
-    Formula(A, F * S),
-    Formula(F, q * B * v * sin(a)),
-    Formula(A, F * h)  # Another hack, because of ambiguity.
+formulas_bare = [
+    (P, (I ** 2) * R),
+    (v, S / t),
+    (v, lam / T),
+    (M, F * d),
+    (ro, m / V),
+    (F, B * I * l * sin(a)),
+    (E, I / (h ** 2)),
+    (T, t / N),
+    (nu, N / t),
+    (T, 1 / nu),
+    (nu, 1 / T),
+    (F, m * g),
+    (P, m * g),
+    (p, F / S),
+    (A, F * S),
+    (F, q * B * v * sin(a)),
+    (A, F * h),  # Another hack, because of ambiguity.
+    (n, sol / v),
+    (E, m * (sol ** 2)),
+    (m, plank * v / sol ** 2),
+    (m, plank / (sol * lam)),
+    (p, m * sol),
+    (E, plank * sol / lam),
+    (v, 2 * pi * R / T),
+    (nu, 1 / T),
+    (v, 2 * pi * nu),
+    (omega, phi / t),
+    (omega, 2 * pi / T),
+    (omega, 2 * pi * nu),
+    (v, omega * R),
+    (F, -k * x),
+    (F, -k * S),
+    (h, v * t + g * t * t / 2),
+    (v, sqrt(g * R)),
+    (A, F * S * cos(a)),
+    (N, A / t),
+    (E, m * v * v / 2),
+    (E, m * g * h),
+    (E, k * x * x / 2),
+    (p, ro * g * h),
+    (F, ro * g * V),
+    (I, U / R),
+    (B, M / (I * S)),
+    (P, U * U / R),
+    (P, I * U),
+    (Phi, B * S * cos(a)),
+    (F, B * I * l * sin(a)),
 ]
+
+context_formulas = [
+    Formula(D, 1 / F, context={'converging'}),
+    Formula(S, S ** 2, context={'square'}),  # A hack.
+]
+
+formulas = [Formula(*args) for args in formulas_bare] + context_formulas
--- physics_solver/math/variables.py
+++ physics_solver/math/variables.py
@@ -1,7 +1,10 @@
 import sympy
-from sympy.physics.units import newton, kilogram
+from sympy.physics.units import newton, kilogram, joule, second, meter
 
-lam, M, F, l, t, v, S, ro, m, V, d, P, c, R, I, B, E, h, T, nu, A, p, D, N, a, q \
-    = sympy.symbols('\\lambda M F l t v S \\rho m V d P c R I B E h T \\nu A p D N a q')
+lam, M, F, l, t, v, S, ro, m, V, d, P, c, R, I, B, E, h, T, nu, A, p, D, N, a, q, omega, phi, k, x, U, Phi \
+    = sympy.symbols('\\lambda M F l t v S \\rho m V d P c R I B E h T \\nu A p D N a q \\omega \\phi k x U \\Phi')
 
 g = 9.8 * newton / kilogram
+plank = 6.63 * (10 ** (-34)) * joule * second
+sol = 3 * (10 ** 8) * meter / second
+pi = 3.14
