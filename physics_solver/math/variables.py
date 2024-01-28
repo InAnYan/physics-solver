7deb0d86085aff80940efe472fece1ac30d0d038
ruslan <ruslanpopov1512@gmail.com>
2024-01-28 14:33:10 +0200

    added new formulas

--- physics_solver/math/variables.py
+++ physics_solver/math/variables.py
@@ -1,8 +1,8 @@
 import sympy
 from sympy.physics.units import newton, kilogram, joule, second, meter
 
-lam, M, F, l, t, v, S, ro, m, V, d, P, c, R, I, B, E, h, T, nu, A, p, D, N, a, q, omega, phi, k, x, U, Phi \
-    = sympy.symbols('\\lambda M F l t v S \\rho m V d P c R I B E h T \\nu A p D N a q \\omega \\phi k x U \\Phi')
+lam, M, F, l, t, v, S, ro, m, V, d, P, c, R, I, B, E, h, T, nu, A, p, D, N, a, q, omega, phi, k, x, U, Phi, n \
+    = sympy.symbols('\\lambda M F l t v S \\rho m V d P c R I B E h T \\nu A p D N a q \\omega \\phi k x U \\Phi n')
 
 g = 9.8 * newton / kilogram
 plank = 6.63 * (10 ** (-34)) * joule * second
